8ecb944554d1c58bb5815f48c01e4b068d75161a
José Guilherme Sório <josesorio@prefeitura.sp.gov.br>
2024-05-03 10:51:17 -0300

    feat: add flask and flask restful

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self, cpf):
+        return {'cpf': cpf}
+    
+api.add_resource(Users, '/users/<string:cpf>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
